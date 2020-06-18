c3ceeaf84f476f7c75aa8feb8995ca698ce452e6
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-06-18 13:50:59 -0700

    Added: Test for email samples

---
+++ src/test/java/com/bridegelabz/EmailValdiationTest.java
@@ -0,0 +1,58 @@
+package com.bridgelabz;
+
+import com.bridegelabz.UserValidator;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class EmailValdiationTest {
+    private String emailId;
+    private boolean result;
+    private UserValidator userValidator;
+
+    public EmailValdiationTest(String emailId, boolean result) {
+        this.emailId = emailId;
+        this.result = result;
+    }
+
+    @Before
+    public void initialize() {
+        userValidator = new UserValidator();
+    }
+
+    @Parameterized.Parameters
+    public static Collection input() {
+        return Arrays.asList(new Object[][]{{"abc@yahoo.com", true},
+                {"abc-100@yahoo.com", true},
+                {"abc.100@yahoo.com", true},
+                {"abc111@abc.com", true},
+                {"abc-100@abc.net", true},
+                {"abc.100@abc.com.au", true},
+                {"abc@1.com", true},
+                {"abc@gmail.com.com", true},
+                {"abc-gmail.com", false},
+                {"abc@.com.my", false},
+                {"abc123@gmail.a", false},
+                {"abc123@.com", false},
+                {"abc123@.com.com", false},
+                {".abc@abc.com", false},
+                {"abc()*@gmail.com", false},
+                {"abc@%*.com", false},
+                {"abc..2002@gmail.com", false},
+                {"abc.@gmail.com", false},
+                {"abc@abc@gmail.com", false},
+                {"abc@gmail.com.1a", false},
+                {"abc@gmail.com.aa.au", false}});
+    }
+
+    @Test
+    public void testEmailValidation() {
+        Assert.assertEquals(UserValidator.emailPattern, result, UserValidator.userDataValidation(emailId,UserValidator.emailPattern));
+    }
+}
