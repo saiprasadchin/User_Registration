8c6d546a40f1e38599445f8b05279fd401168e26
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-06-18 12:01:30 -0700

    Refactor use proper naming convention

--- src/test/java/com/bridegelabz/userValidationTest.java
+++ src/test/java/com/bridegelabz/userValidationTest.java
@@ -11,4 +11,27 @@ public class userValidationTest {
         Assert.assertTrue(result);
     }
 
+    @Test
+    public void givenFirstNameWhenAllLowerCaseShouldReturnFalse() {
+        boolean result = UserValidator.userDataValidation("saiprasad",UserValidator.namePattern);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenFirstNameWhenTwoCharacterPresentShouldReturnFalse() {
+        boolean result = UserValidator.userDataValidation("sa",UserValidator.namePattern);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenFirstNameWhenAllUpperCaseShouldReturnFalse() {
+        boolean result = UserValidator.userDataValidation("SAIPRASAD",UserValidator.namePattern);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenFirstName_whenDigitsEntered_ShouldReturnFalse() {
+        boolean result = UserValidator.userDataValidation("1234",UserValidator.namePattern);
+        Assert.assertFalse(result);
+    }
 }
